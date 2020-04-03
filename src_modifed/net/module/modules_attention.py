eb053687478cdb2d10596ce74e63dffb1f43f85a
hebowei2000 <2312217486@qq.com>
2020-04-03 16:43:56 +0800

    invalid syntax

--- src_modifed/net/module/modules_attention.py
+++ src_modifed/net/module/modules_attention.py
@@ -1205,7 +1205,7 @@ class ChannelAttention(nn.Module):
 class SpatialAttention(nn.Module):
     def __init__(self,kernel_size=7):
         super(SpatialAttention,self).__init__()
-        assert kernel_size in (3,7),
+        assert kernel_size in (3,7)
         padding = 3 if kernel_size = 7 else 1
 
         self.conv = nn.Conv2d(2,1,kernel_size,padding = padding, bias = False)
